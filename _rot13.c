786cd35c76fa2a28e2ab4fbf683a921b0ec65fa1
alina-delossantos <alina.delossantos@gmail.com>
2021-03-16 19:59:36 +0000

    added rot13

---
+++ _rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _rot13 - prints the rot13'ed string
+ * @rot: list
+ * Return: int
+ */
+
+int _rot13(va_list rot)
+{
+	int i, j;
+	int counter = 0;
+	char *s = va_arg(arg, char *)
+	char letters[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[53] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (s[i] != '\0')
+	{
+		for (counter = 0; counter < 52; counter++)
+			if (s[counter] == letters[i])
+			{
+				_putchar(rot13[j]);
+				counter++;
+			}
+			else
+			{
+				_putchar(s[i]);
+				counter++;
+			}
+		return(counter);
+}
