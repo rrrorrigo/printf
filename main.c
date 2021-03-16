f8e37dda2d4181a7858acbabd7eb2d018ca28774
rrrorrigo <rodrigobriones@gmail.com>
2021-03-16 14:10:26 +0000

    hot fix

--- main.c
+++ main.c
@@ -13,11 +13,13 @@ int main(void)
 	unsigned int ui;
 	void *addr;
 
-	len = _printf("Let's try to printf a simple sentence.\n");
+	len = _printf("hello\n");
 	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
-
-
+	_printf("AA % \n", NULL);
+	_printf("\0");
+	_printf("\n");
+	_printf("\\");
 	_printf("Negative:[%d]\n", -762534);
 
 	_printf("Length:[%d, %i]\n", len, len);
